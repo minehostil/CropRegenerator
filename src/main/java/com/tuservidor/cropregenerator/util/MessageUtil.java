8007936eb730b56c36273ae01d46d918ca12c40b
Minehostil <108409110+minehostil@users.noreply.github.com>
2026-05-04 19:00:26 -0600

    Add MessageUtil class for message handling
    Implement MessageUtil class for sending configurable messages with MiniMessage support.

---
+++ src/main/java/com/tuservidor/cropregenerator/util/MessageUtil.java
@@ -0,0 +1,33 @@
+package com.tuservidor.cropregenerator.util;
+
+import com.tuservidor.cropregenerator.CropRegeneratorPlugin;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.command.CommandSender;
+
+/**
+ * EnvÃ­a mensajes configurables con soporte MiniMessage y reemplazos.
+ */
+public class MessageUtil {
+
+    private static final MiniMessage MM = MiniMessage.miniMessage();
+
+    /**
+     * EnvÃ­a el mensaje del config al sender.
+     * @param replacements pares: "{placeholder}", "valor"
+     */
+    public static void send(CommandSender sender, String key, String... replacements) {
+        CropRegeneratorPlugin plugin = CropRegeneratorPlugin.getInstance();
+        String prefix = plugin.getConfig().getString("messages.prefix", "");
+        String msg    = plugin.getConfig().getString("messages." + key, key);
+
+        for (int i = 0; i + 1 < replacements.length; i += 2) {
+            msg = msg.replace(replacements[i], replacements[i + 1]);
+        }
+
+        sender.sendMessage(MM.deserialize(prefix + msg));
+    }
+
+    public static void send(CommandSender sender, String key) {
+        send(sender, key, new String[0]);
+    }
+}
