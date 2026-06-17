d3b4cb5ef7f0b1f800095dcd5029d0150fcae402
Minehostil <108409110+minehostil@users.noreply.github.com>
2026-06-16 22:32:28 -0600

    Actualizar HologramManager.java

--- src/main/java/com/tuservidor/cropregenerator/managers/HologramManager.java
+++ src/main/java/com/tuservidor/cropregenerator/managers/HologramManager.java
@@ -3,7 +3,6 @@ package com.tuservidor.cropregenerator.managers;
 import com.tuservidor.cropregenerator.CropRegeneratorPlugin;
 import com.tuservidor.cropregenerator.hologram.NativeHologramProvider;
 import com.tuservidor.cropregenerator.model.RegeneratorBlock;
-import org.bukkit.Chunk;
 
 /**
  * Fachada que delega al NativeHologramProvider (TextDisplay de Paper).
@@ -16,9 +15,6 @@ public class HologramManager {
         this.provider = new NativeHologramProvider(plugin);
         plugin.getLogger().info("[HologramManager] Proveedor: Native (TextDisplay)");
     }
-
-    public void purgeAll()              { provider.purgeAll(); }
-    public void purgeChunk(Chunk chunk) { provider.purgeChunk(chunk); }
 
     public void spawnOrUpdate(RegeneratorBlock rb) { provider.spawnOrUpdate(rb); }
     public void updateText(RegeneratorBlock rb)    { provider.updateText(rb); }
